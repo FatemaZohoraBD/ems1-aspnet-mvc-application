a6f779e159e27967a6e9d0523d09463f0953926f
Russell Hussain <Russell Hussain@DESKTOP-N1TOTVU>
2024-03-22 01:06:35 -0700

    Add project files.

---
+++ TaskManagmentSystem/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace TaskManagmentSystem.Models
+{
+    public class Employee : UserActivity
+    {
+        public int Id { get; set; }
+        public int EmpNo { get; set; }
+        public string FirstName { get; set; } = "null";
+        public string MiddleName { get; set; } = "null";
+        public string LastName { get; set; } = "null";
+        public string FullName => $"{FirstName} {MiddleName} {LastName}";
+        public int PhoneNumber { get; set; } 
+        public string Emailaddress { get; set; } = "null";
+        public string Country { get; set; } = "null";
+        public DateTime DateOfBirth { get; set; } 
+        public string Address { get; set; } = "null";
+        public string Department { get; set; } = "null";
+        public string Designation { get; set; } = "null";
+
+
+    }
+
+}
